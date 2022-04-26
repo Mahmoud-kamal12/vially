2ec81105b49cf6ce12b79e0dd0625b6b7f08b5b2
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2022-04-26 02:54:59 +0200

    fix srcoll out of the page

--- item_analysis.php
+++ item_analysis.php
@@ -65,86 +65,88 @@ $id = Trim(stripslashes($_GET['id']));
         ?>
     </div>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
 
-                <div class="panel-heading">
+                    <div class="panel-heading">
 
-                    <div class="btn-group add-new">
-                        <button class="btn btn-xs btn-primary" style="float:right;"
-                            onclick="openPrint('<?= $url ?>')">طباعه</button>
+                        <div class="btn-group add-new">
+                            <button class="btn btn-xs btn-primary" style="float:right;"
+                                onclick="openPrint('<?= $url ?>')">طباعه</button>
+                        </div>
+
+                        <div class="btn-group add-new">
+                            <a href="<?php echo getenv("HTTP_REFERER"); ?>"><button class="btn btn-xs btn-primary"
+                                    style="float:right;">عودة الى الاصناف</button></a>
+                        </div>
                     </div>
 
-                    <div class="btn-group add-new">
-                        <a href="<?php echo getenv("HTTP_REFERER"); ?>"><button class="btn btn-xs btn-primary"
-                                style="float:right;">عودة الى الاصناف</button></a>
-                    </div>
-                </div>
+                    <div class="panel-body">
+                        <div class="table-responsive">
 
-                <div class="panel-body">
-                    <div class="table-responsive">
+                            <div class="scrollable">
+                                <table class="table table-striped table-bordered table-hover" id="table_print">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-center" colspan="15"><?php echo "$code_lang"; ?> :
+                                                <?php echo Get_model_name($_GET['id']); ?></th>
 
-                        <div class="scrollable">
-                            <table class="table table-striped table-bordered table-hover" id="table_print">
-                                <thead>
-                                    <tr>
-                                        <th class="text-center" colspan="15"><?php echo "$code_lang"; ?> :
-                                            <?php echo Get_model_name($_GET['id']); ?></th>
-
-                                    </tr>
-                                    <tr>
+                                        </tr>
+                                        <tr>
 
 
-                                        <th class="text-center"><?php echo "$color_lang"; ?></th>
-                                        <?php
-                                        if ($use_sizes == 1) {
-                                            $result_sizes = @mysqli_query($con, "SELECT * FROM sizes where id in (" . rtrim(get_sizes_of_item($_GET['id']), ",") . ")  order by id ASC limit 0,15");
-                                            $num_sizes = @mysqli_num_rows($result_sizes);
-                                            if ($num_sizes > 0) {
-
-                                                while ($row_sizes = mysqli_fetch_array($result_sizes)) {
-                                                    echo '<th class="text-center">';
-                                                    echo $row_sizes['size'];
-                                                    echo '<a href="barcodes_labels.php?code=' . $_GET['id'] . '&sizeid=' . $row_sizes['id'] . '"><i class="fa fa-barcode fa-xs text-danger" style="float:left"></i></a>';
-                                                    echo '</th>';
-                                                    //  $sizesarray[]=$row_sizes['size'];
-                                                }
-                                            }
-                                        }
-                                        ?>
-
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    $result_colors = @mysqli_query($con, "SELECT * FROM colors where id in (" . rtrim(get_clolors_of_item($_GET['id']), ",") . ")   order by id ASC");
-                                    $num_colors = @mysqli_num_rows($result_colors);
-                                    if ($num_colors > 0) {
-                                        while ($row_colors = mysqli_fetch_array($result_colors)) {
-                                            $color_db = $row_colors['color'];
-                                            echo "<tr>";
-                                            echo '<td class="text-center">' . $row_colors['color'] . '</td>';
-                                            if ($use_sizes == "1") {
-                                                $result_sizes = @mysqli_query($con, "SELECT * FROM sizes where id in (" . rtrim(get_sizes_of_item($_GET['id']), ",") . ") order by id ASC limit 0,15");
+                                            <th class="text-center"><?php echo "$color_lang"; ?></th>
+                                            <?php
+                                            if ($use_sizes == 1) {
+                                                $result_sizes = @mysqli_query($con, "SELECT * FROM sizes where id in (" . rtrim(get_sizes_of_item($_GET['id']), ",") . ")  order by id ASC limit 0,15");
                                                 $num_sizes = @mysqli_num_rows($result_sizes);
                                                 if ($num_sizes > 0) {
 
                                                     while ($row_sizes = mysqli_fetch_array($result_sizes)) {
-                                                        echo '<td class="text-center">' . Get_total_items('' . $row_sizes['id'] . '', '' . $row_colors['id'] . '', $_GET['id']) . '</td>';
+                                                        echo '<th class="text-center">';
+                                                        echo $row_sizes['size'];
+                                                        echo '<a href="barcodes_labels.php?code=' . $_GET['id'] . '&sizeid=' . $row_sizes['id'] . '"><i class="fa fa-barcode fa-xs text-danger" style="float:left"></i></a>';
+                                                        echo '</th>';
+                                                        //  $sizesarray[]=$row_sizes['size'];
                                                     }
                                                 }
-                                            } else {
-                                                echo '<td class="text-center">' . Get_total_items('0', '' . $row_colors['id'] . '', $_GET['id']) . '</td>';
                                             }
-                                            echo "</tr>";
-                                        }
-                                    }
-                                    ?>
-                                </tbody>
-                            </table>
-                        </div>
+                                            ?>
 
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $result_colors = @mysqli_query($con, "SELECT * FROM colors where id in (" . rtrim(get_clolors_of_item($_GET['id']), ",") . ")   order by id ASC");
+                                        $num_colors = @mysqli_num_rows($result_colors);
+                                        if ($num_colors > 0) {
+                                            while ($row_colors = mysqli_fetch_array($result_colors)) {
+                                                $color_db = $row_colors['color'];
+                                                echo "<tr>";
+                                                echo '<td class="text-center">' . $row_colors['color'] . '</td>';
+                                                if ($use_sizes == "1") {
+                                                    $result_sizes = @mysqli_query($con, "SELECT * FROM sizes where id in (" . rtrim(get_sizes_of_item($_GET['id']), ",") . ") order by id ASC limit 0,15");
+                                                    $num_sizes = @mysqli_num_rows($result_sizes);
+                                                    if ($num_sizes > 0) {
+
+                                                        while ($row_sizes = mysqli_fetch_array($result_sizes)) {
+                                                            echo '<td class="text-center">' . Get_total_items('' . $row_sizes['id'] . '', '' . $row_colors['id'] . '', $_GET['id']) . '</td>';
+                                                        }
+                                                    }
+                                                } else {
+                                                    echo '<td class="text-center">' . Get_total_items('0', '' . $row_colors['id'] . '', $_GET['id']) . '</td>';
+                                                }
+                                                echo "</tr>";
+                                            }
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
